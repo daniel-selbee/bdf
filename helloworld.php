16d640d87d09d513336bd4c077dd641338b821b1
Daniel Selbee <sulb1210@yahoo.com>
2014-03-11 11:02:51 -0600

    added helloworld.php to the server

--- helloworld.php
+++ helloworld.php
@@ -1,5 +1,9 @@
 <?php
-$hello = 'hello world';
+/* write a variable containing a string */
+$hello = 'Hello world';
+/* write a variable containing the date*/
 $time = date("D M d, Y G:i a");
+/* make them display in the browser with a line break*/
 echo $hello, "<br>", $time;
+
 ?>
