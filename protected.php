4a6475e1db5ebfbd49e50e11f779d66c99292829
Daniel Selbee <sulb1210@yahoo.com>
2014-03-16 16:49:06 -0600

    setup protector.php to go back to home page if not logged in

---
+++ protected.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+include 'models/protector.php';
+
+//my protected controller
+include 'models/viewModel.php';
+include 'models/gameModel.php';
+
+$pagename = 'protected';
+
+$views = new viewModel();
+$games = new gameModel();
+
+$views->getView("views/header.inc");
+
+//Here I want to show the initial list
+if(!empty($_GET["action"])){
+
+}else{
+    //PROTECTED PAGE //CRUD WILL GO IN HERE LATER
+
+}
+//HERE I WNAT TO SHOW THE FOOTER
+$views->getView("views/footer.inc");
