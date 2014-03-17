9d37559943010892a16fa656c80d88279a9b6600
Daniel Selbee <sulb1210@yahoo.com>
2014-03-17 16:21:26 -0600

    removed normalization due to interference with fonts
    added checklogin,  and logout functions to gameModel class

--- models/gameModel.php
+++ models/gameModel.php
@@ -38,5 +38,26 @@ JOIN genreTB
 
         return $st->fetchAll();
     }
+
+    public function checkLogin($uname='', $password=''){
+        $sql = "select * from users where un = uname and pass=:password";
+        $st = $this->db->prepare($sql);
+        $st->execute(array(":uname"=>$uname,":password"=>$password));
+
+        $num = $st->rowCount();
+
+        if($num>-0){
+            $_SESSION["loggedin"] = 1;
+
+        }else{
+            $_SESSION["loggedin"] = 0;
+        }
+
+        return $st->fetchAll(PDO::FETCH_COLUMN, 0);
+    }
+
+    public function logout(){
+        $_SESSION["loggedin"] = 0;
+    }
 }
 ?>
