d5c559266da7a7900b6fb5d3461f616297316f91
Daniel Selbee <sulb1210@yahoo.com>
2014-03-17 15:56:10 -0600

    normalized css

--- models/gameModel.php
+++ models/gameModel.php
@@ -7,16 +7,13 @@ class gameModel extends DB{
     public function getAll(){
 
         $sql =
-           "SELECT games.gameId, games.title, genreTB.genre, platformTB.platform
-             FROM games
-            JOIN genreTB
-                on  games.genreId = genreTB.genreId
-             join  platformTB
-                on games.platformId = platformTB.platformId";
-        /*
-         CAN I HAVE A SECOND QUERY HERE?
-         IF NOT, HOW DO I LINK A 4TH UNRELATED TABLE TO MY LAST QUERY
-         */
+            "SELECT games.gameId, games.title, genreTB.genre, platformTB.platform
+              FROM games
+             JOIN genreTB
+                 on  games.genreId = genreTB.genreId
+              join  platformTB
+                 on games.platformId = platformTB.platformId";
+
 
         $st = $this->db->prepare($sql);
         $st->execute();
